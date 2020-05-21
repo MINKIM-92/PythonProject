c39b494bb8eab6a63eb1951c410169ca5e143838
MINKIM-92 <dhflqksoto@gmail.com>
2020-05-20 23:36:09 -0400

    First step of creating games of python.

---
+++ solution.py
@@ -0,0 +1,12 @@
+import random
+import operator
+
+def random_problem():
+    
+
+    # The dictionary. It defines the whatever 
+    operator = {
+        '+': operator.add
+        '-': operator.sug
+    }
+
