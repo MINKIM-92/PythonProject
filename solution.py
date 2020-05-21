17708020be7bf0cbecd16cdec8775070a11cd14e
MINKIM-92 <dhflqksoto@gmail.com>
2020-05-20 23:56:10 -0400

    game is working

--- solution.py
+++ solution.py
@@ -5,8 +5,46 @@ def random_problem():
     
 
     # The dictionary. It defines the whatever 
-    operator = {
-        '+': operator.add
-        '-': operator.sug
+    operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
     }
+    #generate the random numbers from 1 to 10
+    num_1 = random.randint(1,10)
+    num_2 = random.randint(1,10)
+    operation = random.choice(list(operators.keys()))
+    answer = operators.get(operation)(num_1,num_2)
+    print(f'what is {num_1}{operation}{num_2}')
+
+    return answer
+
+
+def ask_question():
+    answer = random_problem()
+    guess = float(input())
+    return guess == answer
+
+def game():
+    print("How well do you know math?\n")
+    score = 0
+    for i in range(5):
+        if ask_question() == True:
+            score += 1
+            print("Correct!")
+        else:
+            print("Incorrect!")
+    print(f'Your score is {score}')
+
+game()
+    
+
+
+
+
+
+
+
+
 
